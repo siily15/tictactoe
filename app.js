809359b8ff262ec337d26226c0576c07515af4c1
siily <arnokosk15@gmail.com>
2020-10-28 08:47:08 +0200

    ai and removed winner for o

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
+//let is gameOver = false
 let gameboard = [
     ['_', '_', '_',],
     ['_', '_', '_',],
     ['_', '_', '_',]
 ]
-
-const winningsSums = [7, 56, 73, 84, 146, 273, 292, 448]
 
 const cellDivs = document.querySelectorAll('.cell');
 
@@ -15,68 +14,85 @@ cellDivs.forEach(element => {
         const selectedCol = selectedCell.dataset.col
 
         if (gameboard[selectedRow][selectedCol] == '_') {
-            selectedCell.innerText = 'o'
+            selectedCell.innerText = 'x'
             gameboard[selectedRow][selectedCol] = 'x'
+            let gameOver = false
+
+             //ai Horizontal
+
+             if (gameboard[0][0] == 'x') {
+                document.getElementById('10').innerText = 'o';
+            }
+            if (gameboard[0][1] == 'x') {
+                document.getElementById('11').innerText = 'o';
+            }
+            if (gameboard[0][2] == 'x') {
+                document.getElementById('12').innerText = 'o';
+            }
+            if (gameboard[1][0] == 'x') {
+                document.getElementById('00').innerText = 'o';
+            }
+            if (gameboard[1][1] == 'x') {
+                document.getElementById('01').innerText = 'o';
+            }
+            if (gameboard[1][2] == 'x') {
+                document.getElementById('02').innerText = 'o';
+            }
+
+            //ai vertigal
+
+            //ai diagonal
 
             //Horizontal
 
             if (gameboard[0][0] == 'x' && gameboard[0][1] == 'x' && gameboard[0][2] == 'x') {
                 console.log('winner')
+            } else {
+                gameOver = true
             }
             if (gameboard[1][0] == 'x' && gameboard[1][1] == 'x' && gameboard[1][2] == 'x') {
                 console.log('winner')
+            } else {
+                gameOver = true
             }
             if (gameboard[2][0] == 'x' && gameboard[2][1] == 'x' && gameboard[2][2] == 'x') {
                 console.log('winner')
-            }
-            if (gameboard[0][0] == 'o' && gameboard[0][1] == 'o' && gameboard[0][2] == 'o') {
-                console.log('winner')
-            }
-            if (gameboard[1][0] == 'o' && gameboard[1][1] == 'o' && gameboard[1][2] == 'o') {
-                console.log('winner')
-            }
-            if (gameboard[2][0] == 'o' && gameboard[2][1] == 'o' && gameboard[2][2] == 'o') {
-                console.log('winner')
+            } else {
+                gameOver = true
             }
 
             //Vertigal
 
             if (gameboard[0][0] == 'x' && gameboard[1][0] == 'x' && gameboard[2][0] == 'x') {
                 console.log('winner')
+            } else {
+                gameOver = true
             }
 
             if (gameboard[0][1] == 'x' && gameboard[1][1] == 'x' && gameboard[2][1] == 'x') {
                 console.log('winner')
+            } else {
+                gameOver = true
             }
 
             if (gameboard[0][2] == 'x' && gameboard[1][2] == 'x' && gameboard[2][2] == 'x') {
                 console.log('winner')
-            }
-            if (gameboard[0][0] == 'o' && gameboard[1][0] == 'o' && gameboard[2][0] == 'o') {
-                console.log('winner')
-            }
-            if (gameboard[0][1] == 'o' && gameboard[1][1] == 'o' && gameboard[2][1] == 'o') {
-                console.log('winner')
-            }
-            if (gameboard[0][2] == 'o' && gameboard[1][2] == 'o' && gameboard[2][2] == 'o') {
-                console.log('winner')
+            } else {
+                gameOver = true
             }
 
             //Diagonal
 
             if (gameboard[0][0] == 'x' && gameboard[1][1] == 'x' && gameboard[2][2] == 'x') {
                 console.log('winner')
+            } else {
+                gameOver = true
             }
             if (gameboard[0][2] == 'x' && gameboard[1][1] == 'x' && gameboard[2][0] == 'x') {
                 console.log('winner')
+            } else {
+                gameOver = true
             }
-            if (gameboard[0][0] == 'o' && gameboard[1][1] == 'o' && gameboard[2][2] == 'o') {
-                console.log('winner')
-            }
-            if (gameboard[0][2] == 'o' && gameboard[1][1] == 'o' && gameboard[2][0] == 'o') {
-                console.log('winner')
-            }
-            console.log(gameboard)
         }
     })
 });
